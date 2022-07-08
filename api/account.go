2b076ed75706a8efe00b3fc90188180b5ff717c4
Kristina <kristinaa.zvonar@hotmail.com>
2022-07-08 21:25:57 +0200

    Add GET and POST to accounts endpoint

---
+++ api/account.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	db "bank-mvp/db/sqlc"
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shopspring/decimal"
+)
+
+type createAccountRequest struct {
+	Currency string          `json:"currency" binding:"required,oneof=USD EUR"`
+	ClientID int64           `json:"client_id" binding:"required"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams {
+		ClientID: req.ClientID,
+		Currency: req.Currency,
+		Balance: decimal.Zero,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"bank-mvp/api"
+	db "bank-mvp/db/sqlc"
+	"database/sql"
+	"log"
 
+	_ "github.com/lib/pq"
+)
+
+var config map[string]string = map[string]string{
+	"dbDriver": "postgres",
+	"dbSource": "postgresql://root:root@localhost:5432/bank_mvp?sslmode=disable",
+	"serverAddress": "0.0.0.0:8080",
+}
+
+func main() {
+	conn, err := sql.Open(config["dbDriver"], config["dbSource"])
+	if err != nil {
+		log.Fatalln("Cannot connect to DB:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(config["serverAddress"])
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
