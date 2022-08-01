5b2ed3e3d9554e6c1638076aae62f02c395e39b9
Kristina <kristinaa.zvonar@hotmail.com>
2022-08-01 19:23:29 +0200

    Write tests for client.go

--- db/sqlc/client_test.go
+++ db/sqlc/client_test.go
@@ -47,3 +47,49 @@ func TestGetClient(t *testing.T) {
 	require.Equal(t, client1.Active, client2.Active)
 }
 
+func TestListClients(t *testing.T) {
+	clientCnt := util.RandomInt(1, 20)	
+	for i := 1; i < int(clientCnt); i++ {
+		createRandomClient(t)
+	}
+
+	arg := ListClientsParams {
+		Limit: 5,
+		Offset: 0,
+	}
+
+	clients, err := testQueries.ListClients(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, clients)
+	require.True(t, len(clients) >= int(clientCnt))
+
+	for _, client := range clients {
+		require.NotEmpty(t, client)		
+	}
+}
+
+func TestCreateClient(t *testing.T) {
+	createRandomClient(t)
+}
+
+func TestUpdateClient(t *testing.T) {
+	client1 := createRandomClient(t)
+
+	arg := UpdateClientParams {
+		ID: client1.ID,
+		FirstName: util.RandomString(10),
+		LastName: util.RandomString(10),
+		CountryID: util.RandomInt(1, 228),
+		Active: false,
+	}
+
+	client2, err := testQueries.UpdateClient(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, client2)
+
+	require.Equal(t, client1.ID, client2.ID)
+	require.Equal(t, arg.FirstName, client2.FirstName)
+	require.Equal(t, arg.LastName, client2.LastName)
+	require.Equal(t, arg.CountryID, client2.CountryID)
+	require.False(t, client2.Active)
+}
