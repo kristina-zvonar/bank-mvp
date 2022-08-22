4e5c1781a208025811126775bbd2d2d11e74ba69
Kristina <kristinaa.zvonar@hotmail.com>
2022-08-22 07:46:53 +0200

    Add remaining Account tests

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -26,6 +26,43 @@ func createRandomAccount(t *testing.T) Account {
 	return account
 }
 
+func TestAddAccountBalance(t *testing.T) {
+	account1 := createRandomAccount(t)
+	arg := AddAccountBalanceParams{
+		Amount: util.RandomDecimal(1, 100),
+		ID: account1.ID,
+	}
+
+	account2, err := testQueries.AddAccountBalance(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.ClientID, account2.ClientID)
+	require.Equal(t, account1.Active, account2.Active)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.Equal(t, account1.Balance.Add(arg.Amount), account2.Balance)
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.ClientID, account2.ClientID)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.Equal(t, account1.Active, account2.Active)
+	require.Equal(t, account1.Locked, account2.Locked)
+}
+
 func TestListAccount(t *testing.T) {
 	var lastAccount Account
 	for i := 0; i < 10; i++ {
@@ -46,4 +83,25 @@ func TestListAccount(t *testing.T) {
 		require.NotEmpty(t, account)
 		require.Equal(t, lastAccount.ClientID, account.ClientID)
 	}
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	arg := UpdateAccountParams{
+		ID: account1.ID,
+		Balance: account1.Balance,
+		Active: false,
+		Locked: true,
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.ClientID, account2.ClientID)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.Equal(t, arg.Active, account2.Active)
+	require.Equal(t, arg.Locked, account2.Locked)
 }
